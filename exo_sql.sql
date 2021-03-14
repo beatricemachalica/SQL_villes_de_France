5036f36850deebacfb5b953730cd56c907167dc0
beatricemachalica <beatka.macha68@yahoo.fr>
2021-03-14 15:05:48 +0100

    first commit

---
+++ exo_sql.sql
@@ -0,0 +1,36 @@
+-- Exercices SQL – Base de données des villes de France
+
+-- https://sql.sh/exercices-sql/villes-de-france
+-- l convient de télécharger les bases de données d’exemples, qui seront utilisées au sein des exercices.
+-- Table “villes de France” (8.4Mo)
+-- Table “départements de France” (7.9Ko)
+
+-- Veuillez trouver les requêtes SQL permettant d’effectuer chacune des demandes suivantes :
+
+-- 1  Obtenir la liste des 10 villes les plus peuplées en 2012
+
+-- 2  Obtenir la liste des 50 villes ayant la plus faible superficie
+
+-- 3  Obtenir la liste des départements d’outres-mer, 
+--   c’est-à-dire ceux dont le numéro de département commencent par “97”
+
+-- 4  Obtenir le nom des 10 villes les plus peuplées en 2012, 
+--   ainsi que le nom du département associé
+
+-- 5  Obtenir la liste du nom de chaque département, 
+-- associé à son code et du nombre de commune au sein de ces département, 
+-- en triant afin d’obtenir en priorité les départements qui possèdent le plus de communes
+
+-- 6  Obtenir la liste des 10 plus grands départements, en terme de superficie
+
+-- 7  Compter le nombre de villes dont le nom commence par “Saint”
+
+-- 8  Obtenir la liste des villes qui ont un nom existants plusieurs fois, 
+-- et trier afin d’obtenir en premier celles dont le nom est le plus souvent utilisé par plusieurs communes
+
+-- 9  Obtenir en une seule requête SQL la liste des villes dont la superficie est supérieur à la superficie moyenne
+
+-- 10  Obtenir la liste des départements qui possèdent plus de 2 millions d’habitants
+
+-- 11  Remplacez les tirets par un espace vide, 
+-- pour toutes les villes commençant par “SAINT-” (dans la colonne qui contient les noms en majuscule)
